8ae8a1e5cae54032d43255ab5f5453205d8a7dbe
Rick Morales <rick.morales7693@gmail.com>
2023-08-18 12:22:36 -0500

    added albums_seeder, select_exercises, update_exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(50) DEFAULT 'NONE',
-    release_date YEAR,
-    sales FLOAT,
-    genre VARCHAR(50) DEFAULT 'NONE',
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL(7,2) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
